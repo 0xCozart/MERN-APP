ab4896243651ad6ecca0e09a6a75b8d29ef885d0
alan <alanvega002@gmail.com>
2020-07-03 06:11:34 +0000

    basic server needs types

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,4 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express, { Request, Response, Application } from 'express';
 import bodyParser from 'body-parser';
-import moongoose from 'moongoose';
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(':)');
+});
+
+export default app;
